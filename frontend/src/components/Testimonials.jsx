ba62eb2b045ce08595f33dfcf708d7788ffc285d
Mohamed Khaled <tmtmgames2021@gmail.com>
2023-03-08 17:44:24 +0200

    add contet

--- frontend/src/components/Testimonials.jsx
+++ frontend/src/components/Testimonials.jsx
@@ -2,19 +2,18 @@ import { Avatar, Rating, Typography } from "@mui/material";
 import { Stack } from "@mui/system";
 import React from "react";
 
-function Testimonials() {
+function Testimonials({ testimon }) {
     return (
         <>
             <Stack direction="row" gap={3}>
                 <Avatar sx={{ width: 60, height: 60 }} />
                 <Stack>
                     <Typography gutterBottom variant="h5" component="h5">
-                        محمد خالد
+                        {testimon.name}
                     </Typography>
                     <Rating name="read-only" value={5} readOnly />
                     <Typography gutterBottom variant="h6" component="h5">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit. Facilis, corporis.
+                        {testimon.desc}
                     </Typography>
                 </Stack>
             </Stack>
--- frontend/src/components/TestimonialsList.jsx
+++ frontend/src/components/TestimonialsList.jsx
@@ -2,18 +2,29 @@ import { Grid } from "@mui/material";
 import React from "react";
 import Testimonials from "./Testimonials";
 
+const testimonis = [
+    {
+        name: "rashed alotaibi",
+        desc: "تعاملهم جدا رائع وسريعين فالخدمه كانت من اجمل السفرات اللي سفرتها مهتمين فالعميل بادق التفاصيل اشكركم.",
+    },
+    {
+        name: "Abeer Al-Juhani",
+        desc: "شركة ممتازه وموظفين محترمين جدا جزاهم الله خيرا.",
+    },
+    {
+        name: "Abdullah Alshehri",
+        desc: "السلام عليكم تجربتي كانت مع الشركة ممتازة مصداقيه وتعامل راقي.",
+    },
+];
+
 function TestimonialsList() {
     return (
         <Grid container spacing={3}>
-            <Grid item xs={12} md={4}>
-                <Testimonials />
-            </Grid>
-            <Grid item xs={12} md={4}>
-                <Testimonials />
-            </Grid>
-            <Grid item xs={12} md={4}>
-                <Testimonials />
-            </Grid>
+            {testimonis.map((testimon) => (
+                <Grid item xs={12} md={4}>
+                    <Testimonials testimon={testimon} />
+                </Grid>
+            ))}
         </Grid>
     );
 }
